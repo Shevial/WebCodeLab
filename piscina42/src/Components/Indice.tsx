7f8ecca90c5f53e5ac71427fa5f880eab2430be9
Sara HL <sarahevialopez@gmail.com>
2025-03-03 14:34:04 +0100

    Modify

--- piscina42/src/Components/Indice.tsx
+++ piscina42/src/Components/Indice.tsx
@@ -4,7 +4,7 @@ import './Styles/Indice.css'
 function Indice() {
   return (
     <>
-    <div>Proyecto</div>
+    <div>Proyecto !</div>
     <ul>
         <li><a href=''> C00 </a></li>
         <li><a href=''> C01 </a></li>
